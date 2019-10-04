8e1e72e13123b17561697fb14e11fcb47dc179b5
jw <betrayers000@gmail.com>
2019-10-04 11:11:44 +0900

    new & create

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
+
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'index.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    author = request.POST.get("author")
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    due_date = request.POST.get("due-date")
+    
+    todo = Todo.objects.create(
+        author=author, 
+        title=title, 
+        content=content, 
+        due_date=due_date
+        )
+    return redirect('/todos')
+    
+
